84edf0734bc6932e25a918a3897775c31331ba8e
sunnyRK <radadiyasunny970@gmail.com.com>
2020-06-21 16:21:02 +0530

    Change server runnig script

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const routes = require("./routes");
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    createServer(handler).listen(3000,(err) => {
+    createServer(handler).listen(process.env.PORT || 3000,(err) => {
         if(err) throw err;
-        console.log("Ready on localhost:3000");
+        console.log(`Ready on localhost ${process.env.PORT}`);
+        // `Ready on localhost ${process.env.PORT}`
     });
 });
